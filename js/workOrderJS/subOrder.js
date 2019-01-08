36059fad1c688eb032f097461503c9a6ae7d4f31
longsenli <llsbenign@163.com>
2019-01-08 15:36:09 +0800

    添加扫码领料

--- js/workOrderJS/subOrder.js
+++ js/workOrderJS/subOrder.js
@@ -2,7 +2,6 @@ function subOrderIndustrialPlantSlctFun() {
 	$.ajax({
 		url: window.serviceIP + "/api/basicdata/getindustrialplant",
 		type: "GET",
-
 		contentType: "application/json",
 		dataType: "json",
 		//		headers: {
@@ -12,11 +11,13 @@ function subOrderIndustrialPlantSlctFun() {
 		success: function(dataRes) {
 
 			$("#industrialPlantSlct").find('option').remove();
-			//console.log(dataRes);
+
 			if(dataRes.status == 1) { 
 				var models = eval("(" + dataRes.data + ")");
+				console.log(models);
 				for (var  i  in  models)  {  
-					$('#industrialPlantSlct').append(("<option value=" + models[i].id + ">" + models[i].name+ "</option>").toString())
+					$('#industrialPlantSlct').append(("<option value=" + models[i].id.toString() + ">" +
+						models[i].name.toString() + "</option>").toString())
 				}
 				$('#industrialPlantSlct').selectpicker('refresh');
 				$('#industrialPlantSlct').selectpicker('render');   
@@ -46,8 +47,7 @@ function subOrderProductionProcessSlctFun() {
 			if(dataRes.status == 1) { 
 				var models = eval("(" + dataRes.data + ")");
 				for (var  i  in  models)  {  
-					$('#productionProcessSlct').append(("<option value=" + models[i].id +
-						">" + models[i].name  + "</option>").toString())
+					$('#productionProcessSlct').append(("<option value=" + models[i].id + ">" + models[i].name  + "</option>").toString())
 				}
 				//console.log($('#productionProcessSlct'));
 				$('#productionProcessSlct').selectpicker('refresh');
@@ -86,7 +86,7 @@ function subOrderProductionLineSlctFun() {
 				var models = eval("(" + dataRes.data + ")");
 				for (var  i  in  models)  {  
 					$('#productionLineSlct').append(("<option value=" + models[i].id +
-						">" + models[i].name  + "</option>").toString());
+						">" + models[i].name + "</option>").toString());
 				}
 				$('#productionLineSlct').selectpicker('refresh');
 				$('#productionLineSlct').selectpicker('render');   
@@ -189,7 +189,7 @@ function FinishSubOrder() {
 function SelectWorkOrderFun() {
 	SelectSubOrder();
 	SelectMaterialRecord();
-}
+};
 
 function SelectSubOrder() {
 	var columnsArray = [];
@@ -254,7 +254,7 @@ function SelectSubOrder() {
 				$('#table').bootstrapTable('destroy').bootstrapTable({
 					data: models,
 					toolbar: '#toolbar',
-					singleSelect: true,
+					singleSelect: false,
 					clickToSelect: true,
 					sortName: "orderSplitid",
 					sortOrder: "asc",
@@ -468,7 +468,7 @@ function getUsableMaterialFun() {
 			}
 		}
 	});
-}
+};
 
 function gainMaterialRecord() {
 	var formData = new FormData();
@@ -502,7 +502,7 @@ function gainMaterialRecord() {
 			}
 		}
 	});
-}
+};
 
 function createQRCode() {
 	var row = $.map($('#table').bootstrapTable('getSelections'), function(row) {
@@ -523,18 +523,29 @@ function createQRCode() {
 }
 
 function printQRCode() {
-	createQRCode();
-	var img = document.getElementById("QRImage"); /// get image element
-	var canvas = document.getElementsByTagName("canvas")[0]; /// get canvas element
-	img.src = canvas.toDataURL("image/png"); /// update image
-	var LODOP = getLodop(document.getElementById('LODOP_OB'), document
+	//createQRCode();
+	//	var img = document.getElementById("QRImage"); /// get image element
+	//	var canvas = document.getElementsByTagName("canvas")[0]; /// get canvas element
+	//	img.src = canvas.toDataURL("image/png"); /// update image
+	
+		var selectRow = $("#table").bootstrapTable('getSelections');
+	//var arrayObj = new Array();
+	for(var i = 0; i < selectRow.length; i++)
+	{
+		console.log("dayin");
+
+		var LODOP = getLodop(document.getElementById('LODOP_OB'), document
 		.getElementById('LODOP_EM'));
 	LODOP.PRINT_INIT("打印任务名"); //首先一个初始化语句
 	//LODOP.ADD_PRINT_BARCODE(0,0,200,100,"Code39","*123ABC4567890*");
-	LODOP.ADD_PRINT_BARCODE(0, 0, 200, 100, "QRCode", "*123ABC4567890*");
+	LODOP.ADD_PRINT_BARCODE(0, 0, 200, 200, "QRCode", selectRow[i].id);
+	LODOP.ADD_PRINT_TEXT(205,10,200,100,selectRow[i].ordersplitid);//增加纯文本项
 	//LODOP.ADD_PRINT_HTM(5, 5, 200, 200, document.getElementById("QRImage")) //增加超文本项
 	//LODOP.PREVIEW();
 	LODOP.PRINT(); //最后一个打印(或预览、维护、设计)语句
+	}
+	
+	
 	//document.getElementById("QRImage").style.display="block";
 	//	//document.getElementById("QRImage").ExecWB(6,2);
 	//	$("#QRImage").jqprint({
@@ -548,117 +559,176 @@ function printQRCode() {
 
 var canvas = null,
 	context = null,
-	video = null;   
-window.addEventListener("DOMContentLoaded", function() {       
-	try {           
-		canvas = document.getElementById("canvas");           
-		context = canvas.getContext("2d");           
-		video = document.getElementById("video");           
-		var videoObj = {
-				"video": true,
-				audio: false
-			},
-			               flag = true,
-			               MediaErr = function(error) {                   
-				flag = false;                   
-				if(error.PERMISSION_DENIED) {                       
-					alert('用户拒绝了浏览器请求媒体的权限', '提示');                   
-				} else if(error.NOT_SUPPORTED_ERROR) {                       
-					alert('对不起，您的浏览器不支持拍照功能，请使用其他浏览器', '提示');                   
-				} else if(error.MANDATORY_UNSATISFIED_ERROR) {                       
-					alert('指定的媒体类型未接收到媒体流', '提示');                   
-				} else {                       
-					alert('系统未能获取到摄像头，请确保摄像头已正确安装。或尝试刷新页面，重试', '提示');                   
-				}               
-			};            //获取媒体的兼容代码，目前只支持（Firefox,Chrome,Opera）
-		           
-		if(navigator.getUserMedia) {                //qq浏览器不支持
-			               
-			if(navigator.userAgent.indexOf('MQQBrowser') > -1) {                   
-				alert('对不起，您的浏览器不支持拍照功能，请使用其他浏览器', '提示');                   
-				return false;               
-			}               
-			navigator.getUserMedia(videoObj, function(stream) {                   
-				video.src = stream;                   
-				video.play();               
-			}, MediaErr);           
-		}           
-		else if(navigator.webkitGetUserMedia) {               
-			navigator.webkitGetUserMedia(videoObj, function(stream) {                   
-				video.src = window.webkitURL.createObjectURL(stream);                   
-				video.play();               
-			}, MediaErr);           
-		}           
-		else if(navigator.mozGetUserMedia) {               
-			navigator.mozGetUserMedia(videoObj, function(stream) {                   
-				video.src = window.URL.createObjectURL(stream);                   
-				video.play();               
-			}, MediaErr);           
-		}           
-		else if(navigator.msGetUserMedia) {               
-			navigator.msGetUserMedia(videoObj, function(stream) {                   
-				$(document).scrollTop($(window).height());                   
-				video.src = window.URL.createObjectURL(stream);                   
-				video.play();               
-			}, MediaErr);           
-		} else {               
-			alert('对不起，您的浏览器不支持拍照功能，请使用其他浏览器');               
-			return false;           
-		}           
-		if(flag) {                // alert('为了获得更准确的测试结果，请尽量将二维码置于框中，然后进行拍摄、扫描。 请确保浏览器有权限使用摄像功能');
-			           }            //这个是拍照按钮的事件，
-		           
-		$("#snap").click(function() {
-			startPat();
-		}).show();       
-	} catch(e) {           
-		//printHtml("浏览器不支持HTML5 CANVAS");       
-	}   
-}, false);    //打印内容到页面
-   
+	video = null;  
+var mediaStreamTrack = null;   
 function printHtml(content) {       
 	$(window.document.body).append(content + "<br/>");   
 }    //开始拍照
-   
-function startPat() {  
-	for(var i = 1; i < 100; i++) {
-		var res = "";
-		setTimeout(function() { //防止调用过快
-			       
-			console.log("判断context扫码");    
-			if(context) {
 
-				console.log("扫码画图");           
-				context.drawImage(video, 0, 0, 320, 320);               
-
-				res = CatchCode();           
+function startScanQR() {
+	if(context) {         
+		context.drawImage(video, 0, 0, 320, 320);               
+		if(canvas != null) {            //以下开始编 数据  
+			var imgData = canvas.toDataURL("image/jpeg");            //将图像转换为base64数据
+			qrcode.decode(imgData);             
+			qrcode.callback = function(imgMsg) {
+				if(imgMsg != null && imgMsg.trim().length > 1 && imgMsg.toString().indexOf("error decoding") == -1) {
+					gainMaterialByQR(imgMsg);
+				} else {
+					setTimeout(startScanQR(), 500);
+				}
 			}       
-		}, 500); 
-		if(res.toString().indexOf("error decoding") > -1) {
-			alert("成功!" + res)
+		}          
+	}  
+}
+
+function startsScanQRPat() { 
+	var getQR = false; 
+
+	var finalQR = null;
+	for(var i = 1; i < 1000; i++) { 
+
+		if(context) {         
+			context.drawImage(video, 0, 0, 320, 320);               
+			if(canvas != null) {            //以下开始编 数据  
+				var imgData = canvas.toDataURL("image/jpeg");            //将图像转换为base64数据
+				qrcode.decode(imgData);             
+				qrcode.callback = function(imgMsg) {
+					if(!getQR && imgMsg != null && imgMsg.trim().length > 1 && imgMsg.toString().indexOf("error decoding") == -1) {
+						getQR = true;
+						gainMaterialByQR(imgMsg);
+					}
+				}       
+			}          
+		}       
+		if(getQR) {
 			break;
 		}
-
-	}       
+	}  
+	//console.log("over ---- over");   
 }    //抓屏获取图像流，并上传到服务器
    
 function CatchCode() { 
-	console.log("图层判断");       
+	var resQR = null;
 	if(canvas != null) {            //以下开始编 数据
-		           
-		console.log("图形");   
 		var imgData = canvas.toDataURL("image/jpeg");            //将图像转换为base64数据
-		       
-		//console.log(imgData); // newfile[0]是通过input file上传的二维码图片文件    
 		qrcode.decode(imgData);             
 		qrcode.callback = function(imgMsg) {                  
-			//alert(imgMsg);
-			console.log("imgMsg", imgMsg); 
-			return     imgMsg;           
+			resQR = imgMsg;
 		}       
-	}   
+	} 
+	return resQR;   
 }
 
-function scanQR(){
+function scanQR() {
 	$('#myModal').modal('show');
+	if(context == null) { 
+		//window.addEventListener("DOMContentLoaded", function() {       
+		try {           
+			canvas = document.getElementById("canvas");           
+			context = canvas.getContext("2d");           
+			video = document.getElementById("video");           
+			var videoObj = {
+					"video": true,
+					audio: false
+				},
+				               flag = true,
+				               MediaErr = function(error) {                   
+					flag = false;                   
+					if(error.PERMISSION_DENIED) {                       
+						alert('用户拒绝了浏览器请求媒体的权限', '提示');                   
+					} else if(error.NOT_SUPPORTED_ERROR) {                       
+						alert('对不起，您的浏览器不支持拍照功能，请使用其他浏览器', '提示');                   
+					} else if(error.MANDATORY_UNSATISFIED_ERROR) {                       
+						alert('指定的媒体类型未接收到媒体流', '提示');                   
+					} else {                       
+						alert('系统未能获取到摄像头，请确保摄像头已正确安装。或尝试刷新页面，重试', '提示');                   
+					}               
+				};            //获取媒体的兼容代码，目前只支持（Firefox,Chrome,Opera）
+			           
+			if(navigator.getUserMedia) {                //qq浏览器不支持
+				               
+				if(navigator.userAgent.indexOf('MQQBrowser') > -1) {                   
+					alert('对不起，您的浏览器不支持拍照功能，请使用其他浏览器', '提示');                   
+					return false;               
+				}               
+				navigator.getUserMedia(videoObj, function(stream) { 
+					mediaStreamTrack = stream;                   
+					video.src = stream;                   
+					video.play();
+
+				}, MediaErr);           
+			}           
+			else if(navigator.webkitGetUserMedia) {               
+				navigator.webkitGetUserMedia(videoObj, function(stream) {  
+					mediaStreamTrack = stream;                  
+					video.src = window.webkitURL.createObjectURL(stream);                   
+					video.play();        
+				}, MediaErr);           
+			}           
+			else if(navigator.mozGetUserMedia) {               
+				navigator.mozGetUserMedia(videoObj, function(stream) { 
+					mediaStreamTrack = stream;                   
+					video.src = window.URL.createObjectURL(stream);                   
+					video.play();               
+				}, MediaErr);           
+			}           
+			else if(navigator.msGetUserMedia) {               
+				navigator.msGetUserMedia(videoObj, function(stream) { 
+					mediaStreamTrack = stream;                   
+					$(document).scrollTop($(window).height());                   
+					video.src = window.URL.createObjectURL(stream);                   
+					video.play();               
+				}, MediaErr);           
+			} else {               
+				alert('对不起，您的浏览器不支持拍照功能，请使用其他浏览器');               
+				return false;           
+			}           
+			if(flag) {                // alert('为了获得更准确的测试结果，请尽量将二维码置于框中，然后进行拍摄、扫描。 请确保浏览器有权限使用摄像功能');
+				          }            //这个是拍照按钮的事件，
+			           
+
+			//				$("#snap").click(function() {
+			//					startPat();
+			//				}).show();       
+		} catch(e) {           
+			//printHtml("浏览器不支持HTML5 CANVAS");       
+		}   
+		//}, false);    //打印内容到页面
+	} 
+	console.log("start");
+	setTimeout(startScanQR(), 10000) ; 
+}
+
+function closeQRScan() {
+	$("#myModal").modal('hide');
+}
+
+function gainMaterialByQR(recordID) {
+	$("#myModal").modal('hide');
+	//console.log("gainMaterialByQR" + recordID);
+	var formData = new FormData();
+	formData.append("expendOrderID", document.PlantToLineSelectForm.workOrderSlct.value.toString());
+	formData.append("outputter", "lls") //$.cookie('username');
+	formData.append("qrCode", "6eb7f3e6705a47fdbe0548a74e63b36f") //$.cookie('username');
+
+	$.ajax({
+		url: window.serviceIP + "/api/material/gainmaterialbyqr",
+		type: "POST",
+		processData: false,
+		contentType: false,
+		data: formData,
+		//		headers: {
+		//			Token: $.cookie('token')
+		//		},
+		//processData: true,
+		success: function(dataRes) {
+			if(dataRes.status == 1) { 
+				getUsableMaterialFun();
+				SelectMaterialRecord();
+			} else {
+				alert("领取失败！" + dataRes.message);
+			}
+		}
+	});
 }
